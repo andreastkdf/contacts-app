f963fe93a01c2da2f8b421eddcd54872a6dddf0f
andreastkdf <andreaskostakis@gmail.com>
2019-04-18 23:38:13 +0100

    setState onNavigate for CreateContact

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,12 +18,12 @@ class ListContacts extends Component {
   }
 
   clearQuery = () => {
-    this.updateQuery('')
+    this.updateQuery("")
   }
 
   render() {
     const { query } = this.state
-    const { contacts, onDeleteContact } = this.props
+    const { contacts, onDeleteContact, onNavigate } = this.props
 
     const showingContacts =
       query === ""
@@ -42,12 +42,18 @@ class ListContacts extends Component {
             value={query}
             onChange={event => this.updateQuery(event.target.value)}
           />
+          <a
+            href='#create'
+            onClick={onNavigate}
+            className='add-contact'
+          >Add Contact</a>
         </div>
 
         {showingContacts.length !== contacts.length && (
-          <div className='showing-contacts'>
-            <span>Now showing {showingContacts.length} of {
-              contacts.length}</span>
+          <div className="showing-contacts">
+            <span>
+              Now showing {showingContacts.length} of {contacts.length}
+            </span>
             <button onClick={this.clearQuery}>Show all</button>
           </div>
         )}
